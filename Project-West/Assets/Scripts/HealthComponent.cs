abf8204e1e4f0f47f602ea5950f8815c23be9da9
Zavier-SE <zavier.se@gmail.com>
2022-11-08 18:12:30 +1100

    Update Player

---
+++ Project-West/Assets/Scripts/HealthComponent.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth;
+    private int currentHealth;
+    public int CurrentHealth
+    {
+        get { return currentHealth; }
+        set { currentHealth = value; }
+    }
+
+    
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void takeDamage(int damage)
+    {
+        if (currentHealth > 0)
+        {
+            currentHealth -= damage;
+        }
+
+        if(currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+    }
+
+    public void recover(int HP)
+    {
+        if (currentHealth < maxHealth && currentHealth > 0)
+        {
+            currentHealth += HP;
+        }
+
+        if(currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+
+    public bool isHealthZero()
+    {
+        if(currentHealth <= 0)
+        {
+            return true;
+        }
+        return false;
+    }
+}
